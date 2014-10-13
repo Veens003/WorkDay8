0fab37f371609bd0150fd3451fff7022dad25f38
idsc3101 <idsc3101@gmail.com>
2014-10-13 17:10:48 -0500

    WD8 Attempt 2

--- ReverseString/ReverseString/Form1.vb
+++ ReverseString/ReverseString/Form1.vb
@@ -3,10 +3,14 @@
 Public Class Form1
 
     Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
-        Dim st As String = txtInput.Text
-
+        txtOutput.Clear()
+        Dim st As String = txtInput.Text.Trim
+        Dim result As String = ""
+        For i = st.Length - 1 To 0 Step -1 'The step -1 is critical as the string wouldn't execute otherwise
+            result = result & st.Chars(i)
+        Next
         ' Please write your code here!
+        txtOutput.Text = result
 
-        txtOutput.Text = "Not implemented yet!"
     End Sub
 End Class
